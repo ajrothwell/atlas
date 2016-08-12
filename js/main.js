ad1a5d759ee8d74a2b7e3137eff1b63d992aeb87
andy.rothwell <rothwell.andy@gmail.com>
2016-08-12 13:12:24 -0400

    used arcgis js api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,81 @@
+
+var CITY_HALL = [-75.163596, 39.952388];
+
+
+require([
+  "esri/Map",
+  "esri/Basemap",
+  "esri/views/MapView",
+  "esri/views/SceneView",
+  "esri/layers/TileLayer",
+  "esri/widgets/BasemapToggle",
+  "dojo/dom",
+  "dojo/on",
+  "dojo/domReady!"
+], function(Map, Basemap, MapView, SceneView, TileLayer, BasemapToggle, dom, on){
+
+
+var tileMapLight = new TileLayer({
+                url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer"
+            });
+
+var baseMapLight = new Basemap({
+baseLayers: [tileMapLight],
+title: "BaseMapLight",
+id: "basemaplight",
+thumbnailUrl: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer/tile/8/96/73"
+});
+
+var tileMapDark = new TileLayer({
+                url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Slate/MapServer"
+            });
+
+var baseMapDark = new Basemap({
+baseLayers: [tileMapDark],
+title: "BaseMapDark",
+id: "basemapdark",
+thumbnailUrl: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Slate/MapServer/tile/2/1546/1186"
+});
+
+var tileMapImagery2015 = new TileLayer({
+                url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2015_3in/MapServer"
+            });
+
+var baseMapImagery2015 = new Basemap({
+baseLayers: [tileMapImagery2015],
+title: "BaseMapImagery2015",
+id: "basemapimagery2015",
+thumbnailUrl: "https://tiles3.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2015_3in/MapServer/tile/18/99254/76343"
+});
+
+
+  var map = new Map({
+    basemap: baseMapLight
+  });
+
+
+  var view = new MapView({
+    container: "map",  // Reference to the scene div created in step 5
+    map: map,  // Reference to the map object created before the scene
+    zoom: 16,  // Sets the zoom level based on level of detail (LOD)
+    center: CITY_HALL  // Sets the center point of view in lon/lat
+  });
+
+  var basemapToggle = new BasemapToggle({
+  view: view,
+  nextBasemap: baseMapDark,
+  nextBasemap: baseMapImagery2015
+  });
+  //map.add(baseMapDark)
+
+  basemapToggle.startup();
+
+  view.ui.add(basemapToggle, "top-right");
+
+});
+
+
+/*
 APP = (function () {
     var map;
     
@@ -74,6 +152,6 @@ APP = (function () {
 $(function () {
     APP.init();
 });
-
+*/
 
 // https://api.tiles.mapbox.com/v4/mapbox.streets/11/1024/681.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw
